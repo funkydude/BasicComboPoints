b22dd1278273eb673f9f3018890b5d36e2180edc
funkydude <funkydude@users.noreply.github.com>
2007-10-16 02:05:42 +0000

    .BasicComboPoints: - initial import - Numerical display of current combo points

---
+++ BasicComboPoints.lua
@@ -0,0 +1,47 @@
+﻿--------------------------------------------------
+----------------BASIC COMBO POINTS----------------
+--------------------------------------------------
+
+local points = nil
+local GetComboPoints = GetComboPoints
+
+--------------------------------------------------
+------------------FRAME CREATION------------------
+--------------------------------------------------
+
+local display = CreateFrame("Frame", "BCP", UIParent)
+display:SetFrameStrata("BACKGROUND")
+display:SetWidth(30)
+display:SetHeight(30)
+display:SetPoint("CENTER", UIParent, "CENTER", 400, -400)
+display:Show()
+
+--------------------------------------------------
+---------------------FONT SET---------------------
+--------------------------------------------------
+
+local text = display:CreateFontString(nil,"OVERLAY")
+text:SetPoint("CENTER", display, "CENTER")
+text:SetFont("Fonts\\FRIZQT__.TTF", 25)
+text:SetText("0")
+text:SetShadowOffset(.8, -.8)
+text:SetShadowColor(0, 0, 0, 1)
+
+--------------------------------------------------
+-------------------EVENT REGISTER-----------------
+--------------------POINT UPDATE------------------
+--------------------------------------------------
+
+local frame = CreateFrame("Frame")
+frame:SetScript("OnEvent", function(frame, event)
+
+points = GetComboPoints()
+text:SetText(points)
+
+--debug
+--ChatFrame1:AddMessage(("-%d-"):format(points))
+
+end)
+frame:RegisterEvent("PLAYER_COMBO_POINTS")
+frame:RegisterEvent("PLAYER_TARGET_CHANGED")
+
