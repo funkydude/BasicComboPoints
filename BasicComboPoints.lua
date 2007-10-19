45c4a7d0c7829cf538ebc70996fdf60336853f84
funkydude <funkydude@users.noreply.github.com>
2007-10-19 22:32:33 +0000

    BasicComboPoints: - Don't load if we are not a rogue - Now sets onto the right of aguf, X-Perl, Perl, PitBull - Grows in size the more points you have - Changes colour according to points

--- BasicComboPoints.lua
+++ BasicComboPoints.lua
@@ -2,8 +2,15 @@
 ----------------BASIC COMBO POINTS----------------
 --------------------------------------------------
 
+if select(2, UnitClass('player')) ~= "ROGUE" then
+	DisableAddOn("BasicComboPoints")
+	return
+end
+
 local points = nil
 local GetComboPoints = GetComboPoints
+local IsAddOnLoaded = IsAddOnLoaded
+local start = nil
 
 --------------------------------------------------
 ------------------FRAME CREATION------------------
@@ -13,8 +20,6 @@ local display = CreateFrame("Frame", "BCP", UIParent)
 display:SetFrameStrata("BACKGROUND")
 display:SetWidth(30)
 display:SetHeight(30)
-display:SetPoint("RIGHT", TargetFrame, "RIGHT")
-display:SetParent(TargetFrame)
 display:Show()
 
 --------------------------------------------------
@@ -23,26 +28,57 @@ display:Show()
 
 local text = display:CreateFontString(nil,"OVERLAY")
 text:SetPoint("CENTER", display, "CENTER")
-text:SetFont("Fonts\\FRIZQT__.TTF", 25)
-text:SetText("0")
 text:SetShadowOffset(.8, -.8)
 text:SetShadowColor(0, 0, 0, 1)
 
 --------------------------------------------------
 -------------------EVENT REGISTER-----------------
---------------------POINT UPDATE------------------
 --------------------------------------------------
 
-local frame = CreateFrame("Frame")
-frame:SetScript("OnEvent", function(frame, event)
+local event = CreateFrame("Frame")
+event:SetScript("OnEvent", function()
 
 points = GetComboPoints()
-text:SetText(points)
 
---debug
---ChatFrame1:AddMessage(("-%d-"):format(points))
+--addon check, change position according to UF addon
+if not start then
+	start = true
+	if IsAddOnLoaded("ag_UnitFrames") then
+		display:SetParent(aUFtarget)
+		display:SetPoint("RIGHT", aUFtarget, "RIGHT", 25, 0)
+	elseif IsAddOnLoaded("PitBull") then
+		display:SetParent(PitBullUnitFrame4)
+		display:SetPoint("RIGHT", PitBullUnitFrame4, "RIGHT", 50, 0)
+	elseif IsAddOnLoaded("XPerl_Target") then
+		display:SetParent(XPerl_Target)
+		display:SetPoint("RIGHT", XPerl_Target, "RIGHT", 60, 0)
+	elseif IsAddOnLoaded("Perl_Target") then
+		display:SetParent(Perl_Target_StatsFrame_CastClickOverlay)
+		display:SetPoint("RIGHT", Perl_Target_StatsFrame_CastClickOverlay, "RIGHT", 40, 0)
+	else
+		display:SetParent(TargetFrame)
+		display:SetPoint("RIGHT", TargetFrame, "RIGHT", 4, 5)
+	end
+	IsAddOnLoaded = nil
+end
 
+--Update point colour and size
+if points == 0 or points == 1 then
+	text:SetFont("Fonts\\FRIZQT__.TTF", 18)
+	text:SetTextColor(1, 1, 1)
+elseif points == 2 or points == 3 then
+	text:SetFont("Fonts\\FRIZQT__.TTF", 22)
+	text:SetTextColor(0, 1, 0)
+elseif points == 4 then
+	text:SetFont("Fonts\\FRIZQT__.TTF", 27)
+	text:SetTextColor(0, 0, 1)
+else
+	text:SetFont("Fonts\\FRIZQT__.TTF", 35)
+	text:SetTextColor(1, 0, 0)
+end
+
+text:SetText(points) --Update point text
 end)
-frame:RegisterEvent("PLAYER_COMBO_POINTS")
-frame:RegisterEvent("PLAYER_TARGET_CHANGED")
+event:RegisterEvent("PLAYER_COMBO_POINTS")
+event:RegisterEvent("PLAYER_TARGET_CHANGED")
 
