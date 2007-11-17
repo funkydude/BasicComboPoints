5042efe7d7f53d64b7ffca95ae1355eff7fc7eca
funkydude <funkydude@users.noreply.github.com>
2007-11-17 13:56:09 +0000

    BasicComboPoints: - v2.0 - No longer relies on unit frame addons - completely independently movable and lockable

--- BasicComboPoints.lua
+++ BasicComboPoints.lua
@@ -1,6 +1,6 @@
-﻿--------------------------------------------------
-----------------BASIC COMBO POINTS----------------
---------------------------------------------------
+﻿------------------------------
+--      Are you local?      --
+------------------------------
 
 local class = select(2, UnitClass("player"))
 if class == "ROGUE" or class == "DRUID" then
@@ -10,83 +10,138 @@ else
 	return
 end
 
-local points = nil
-local GetComboPoints = GetComboPoints
-local IsAddOnLoaded = IsAddOnLoaded
-local start = nil
+local points = GetComboPoints
+local _G = _G
+local display = nil
+local text = nil
+local font = nil
 
---------------------------------------------------
-------------------FRAME CREATION------------------
---------------------------------------------------
+local db
+local defaults = {
+	profile = {
+		x = nil,
+		y = nil,
+		lock = nil,
+	}
+}
 
-local display = CreateFrame("Frame", "BCP", UIParent)
-display:SetFrameStrata("BACKGROUND")
-display:SetWidth(30)
-display:SetHeight(30)
-display:Show()
+local BasicComboPoints = LibStub("AceAddon-3.0"):NewAddon("BasicComboPoints", "AceEvent-3.0")
 
---------------------------------------------------
----------------------FONT SET---------------------
---------------------------------------------------
+------------------------------
+--      Initialization      --
+------------------------------
 
-local text = display:CreateFontString(nil,"OVERLAY")
-text:SetPoint("CENTER", display, "CENTER")
-local font = "Fonts\\FRIZQT__.TTF"
+function BasicComboPoints:OnInitialize()
+	self.db = LibStub("AceDB-3.0"):New("BasicComboPointsDB", defaults)
+	db = self.db.profile
+end
 
---------------------------------------------------
--------------------EVENT REGISTER-----------------
---------------------------------------------------
+------------------------------
+--       Frame Setup        --
+------------------------------
 
-local event = CreateFrame("Frame")
-event:SetScript("OnEvent", function()
+function BasicComboPoints:OnEnable()
+	if not display then
+		display = CreateFrame("Frame", "BCPFrame", UIParent)
+		display:SetBackdrop({bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",})
+		display:SetFrameStrata("BACKGROUND")
+		display:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+		display:SetBackdropColor(1,1,1,1)
+		display:SetWidth(50)
+		display:SetHeight(50)
+		display:Show()
+		display:EnableMouse(true)
+		display:RegisterForDrag("LeftButton")
+		display:SetMovable(true)
+		display:SetScript("OnDragStart", function() this:StartMoving() end)
+		display:SetScript("OnDragStop", function()
+			this:StopMovingOrSizing()
+			self:SavePosition()
+		end)
 
-points = GetComboPoints()
-
---addon check, change position according to UF addon
-if not start then
-	start = true
-	if IsAddOnLoaded("ag_UnitFrames") then
-		display:SetParent(aUFtarget)
-		display:SetPoint("RIGHT", aUFtarget, "RIGHT", 25, 0)
-	elseif IsAddOnLoaded("PitBull") then
-		display:SetParent(PitBullUnitFrame4)
-		display:SetPoint("RIGHT", PitBullUnitFrame4, "RIGHT", 50, 0)
-	elseif IsAddOnLoaded("XPerl_Target") then
-		display:SetParent(XPerl_Target)
-		display:SetPoint("RIGHT", XPerl_Target, "RIGHT", 60, 0)
-	elseif IsAddOnLoaded("Perl_Target") then
-		display:SetParent(Perl_Target_StatsFrame_CastClickOverlay)
-		display:SetPoint("RIGHT", Perl_Target_StatsFrame_CastClickOverlay, "RIGHT", 40, 0)
-	else
-		display:SetParent(TargetFrame)
-		display:SetPoint("RIGHT", TargetFrame, "RIGHT", 4, 5)
+	local x = db.x
+	local y = db.y
+	if x and y then
+		local s = display:GetEffectiveScale()
+		display:ClearAllPoints()
+		display:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", x / s, y / s)
 	end
-	IsAddOnLoaded = nil
+
+	if db.lock then
+		display:SetBackdropColor(1,1,1,0)
+		display:EnableMouse(false)
+		display:SetMovable(false)
+	end
+
+		text = display:CreateFontString(nil,"OVERLAY")
+		text:SetPoint("CENTER", display, "CENTER")
+		font = "Fonts\\FRIZQT__.TTF"
+		text:SetFont(font, 15)
+	end
+
+	self:RegisterEvent("PLAYER_COMBO_POINTS", "Update")
+	self:RegisterEvent("PLAYER_TARGET_CHANGED", "Update")
 end
 
---Update point colour and size
-if points == 0 then
-	text:SetFont(font, 18) 
-	points = "" --remove this line when testing new frame locations for ease, we don't want to display if we have 0 points
-elseif points == 1 then
-	text:SetFont(font, 18)
-	text:SetTextColor(1, 1, 1)
-elseif points == 2 then
-	text:SetFont(font, 22)
-	text:SetTextColor(0, 1, 0)
-elseif points == 3 then
-	text:SetFont(font, 25)
-	text:SetTextColor(1, 1, 0)
-elseif points == 4 then
-	text:SetFont(font, 29)
-	text:SetTextColor(0, 0, 1)
-else
-	text:SetFont(font, 35)
-	text:SetTextColor(1, 0, 0)
+function BasicComboPoints:SavePosition()
+	if not display then return end
+
+	local s = display:GetEffectiveScale()
+	db.x = display:GetLeft() * s
+	db.y = display:GetTop() * s
 end
 
-text:SetText(points) --Update point text
-end)
-event:RegisterEvent("PLAYER_COMBO_POINTS")
-event:RegisterEvent("PLAYER_TARGET_CHANGED")
+------------------------------
+--       Point Update       --
+------------------------------
 
+function BasicComboPoints:Update()
+	local pts = points()
+
+	if pts == 0 then
+		pts = ""
+	elseif pts == 1 then
+		text:SetFont(font, 15)
+		text:SetTextColor(1, 1, 1)
+	elseif pts == 2 then
+		text:SetFont(font, 25)
+		text:SetTextColor(0, 1, 0)
+	elseif pts == 3 then
+		text:SetFont(font, 35)
+		text:SetTextColor(1, 1, 0)
+	elseif pts == 4 then
+		text:SetFont(font, 45)
+		text:SetTextColor(0, 0, 1)
+	else
+		text:SetFont(font, 55)
+		text:SetTextColor(1, 0, 0)
+	end
+
+	text:SetText(pts)
+end
+
+------------------------------
+--     Slash Commands       --
+------------------------------
+
+_G["SlashCmdList"]["BASICCOMBOPOINTS"] = function(msg)
+	if string.lower(msg) == "lock" then
+		if not db.lock then
+			display:SetBackdropColor(1,1,1,0)
+			display:EnableMouse(false)
+			display:SetMovable(false)
+			db.lock = true
+			ChatFrame1:AddMessage("BasicComboPoints: Locked")
+		else
+			display:SetBackdropColor(1,1,1,1)
+			display:EnableMouse(true)
+			display:SetMovable(true)
+			db.lock = nil
+			ChatFrame1:AddMessage("BasicComboPoints: Unlocked")
+		end
+	elseif msg == "" then
+		ChatFrame1:AddMessage("BasicComboPoints: Commands:")
+		ChatFrame1:AddMessage("/bcp lock")
+	end
+end
+_G["SLASH_BASICCOMBOPOINTS1"] = "/bcp"
