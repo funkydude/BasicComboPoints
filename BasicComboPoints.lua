15d5c4fbb02b777b206ed1f7d6e649bccbf35b89
funkydude <funkydude@users.noreply.github.com>
2008-02-06 00:42:19 +0000

    BasicComboPoints: - more plumbing towards selection of font and color - fix externals

--- BasicComboPoints.lua
+++ BasicComboPoints.lua
@@ -13,7 +13,12 @@ end
 local GetComboPoints = GetComboPoints
 local display = nil
 local text = nil
-local font = nil
+
+local font
+local sizeone, sizetwo, sizethree, sizefour, sizefive
+local coloroneR, colortwoR, colorthreeR, colorfourR, colorfiveR
+local coloroneG, colortwoG, colorthreeG, colorfourG, colorfiveG
+local coloroneB, colortwoB, colorthreeB, colorfourB, colorfiveB
 
 local db
 local defaults = {
@@ -21,10 +26,20 @@ local defaults = {
 		x = nil,
 		y = nil,
 		lock = nil,
+
+		font = "Friz Quadrata TT",
+
+		sizeone = 15, sizetwo = 25, sizethree = 35, sizefour = 45, sizefive = 55,
+
+		coloroneR = 1, colortwoR = 0, colorthreeR = 1, colorfourR = 0, colorfiveR = 1,
+		coloroneG = 1, colortwoG = 1, colorthreeG = 1, colorfourG = 0, colorfiveG = 0,
+		coloroneB = 1, colortwoB = 0, colorthreeB = 0, colorfourB = 1, colorfiveB = 0,
 	}
 }
 
 local BasicComboPoints = LibStub("AceAddon-3.0"):NewAddon("BasicComboPoints", "AceEvent-3.0", "AceConsole-3.0")
+local media = LibStub("LibSharedMedia-2.0")
+local tbl = media:List("font")
 
 local function setLock()
 	if not db.lock then
@@ -130,10 +145,13 @@ local function stop()
 end
 
 function BasicComboPoints:OnEnable()
+	self:Refresh()
+
 	if not display then
 		display = CreateFrame("Frame", "BCPFrame", UIParent)
 		display:SetBackdrop({bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",})
 		display:SetFrameStrata("BACKGROUND")
+		display:SetClampedToScreen(true)
 		display:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
 		display:SetBackdropColor(1,1,1,0)
 		display:SetWidth(50)
@@ -156,7 +174,6 @@ function BasicComboPoints:OnEnable()
 
 		text = display:CreateFontString(nil,"OVERLAY")
 		text:SetPoint("CENTER", display, "CENTER")
-		font = "Fonts\\FRIZQT__.TTF"
 		text:SetFont(font, 15)
 	end
 
@@ -176,36 +193,58 @@ end
 --       Point Update       --
 ------------------------------
 
+function BasicComboPoints:Refresh()
+	--using local variables instead of the address to the database
+	--as calling the direct address on a point update isn't
+	--too healthy, especially in a heavy combat situation.
+
+	font = media:Fetch("font", db.font)
+
+	sizeone = db.sizeone
+	sizetwo = db.sizetwo
+	sizethree = db.sizethree
+	sizefour = db.sizefour
+	sizefive = db.sizefive
+
+	coloroneR = db.coloroneR
+	colortwoR = db.colortwoR
+	colorthreeR = db.colorthreeR
+	colorfourR = db.colorfourR
+	colorfiveR = db.colorfiveR
+
+	coloroneG = db.coloroneG
+	colortwoG = db.colortwoG
+	colorthreeG = db.colorthreeG
+	colorfourG = db.colorfourG
+	colorfiveG = db.colorfiveG
+
+	coloroneB = db.coloroneB
+	colortwoB = db.colortwoB
+	colorthreeB = db.colorthreeB
+	colorfourB = db.colorfourB
+	colorfiveB = db.colorfiveB
+end
+
 function BasicComboPoints:Update()
 	local points = GetComboPoints()
 
 	if points == 0 then
 		points = ""
 	elseif points == 1 then
-		text:SetFont(font, 15)
-		text:SetTextColor(1, 1, 1)
-		--text:SetFont(db.font, db.sizeone)
-		--text:SetTextColor(db.colorone)
+		text:SetFont(font, sizeone)
+		text:SetTextColor(coloroneR,coloroneG,coloroneB)
 	elseif points == 2 then
-		text:SetFont(font, 25)
-		text:SetTextColor(0, 1, 0)
-		--text:SetFont(db.font, db.sizetwo)
-		--text:SetTextColor(db.colortwo)
+		text:SetFont(font, sizetwo)
+		text:SetTextColor(colortwoR,colortwoG,colortwoB)
 	elseif points == 3 then
-		text:SetFont(font, 35)
-		text:SetTextColor(1, 1, 0)
-		--text:SetFont(db.font, db.sizethree
-		--text:SetTextColor(db.colorthree)
+		text:SetFont(font, sizethree)
+		text:SetTextColor(colorthreeR,colorthreeG,colorthreeB)
 	elseif points == 4 then
-		text:SetFont(font, 45)
-		text:SetTextColor(0, 0, 1)
-		--text:SetFont(db.font, db.sizefour)
-		--text:SetTextColor(db.colorfour)
+		text:SetFont(font, sizefour)
+		text:SetTextColor(colorfourR,colorfourG,colorfourB)
 	else
-		text:SetFont(font, 55)
-		text:SetTextColor(1, 0, 0)
-		--text:SetFont(db.font, db.sizefive)
-		--text:SetTextColor(db.colorfive)
+		text:SetFont(font, sizefive)
+		text:SetTextColor(colorfiveR,colorfiveG,colorfiveB)
 	end
 
 	text:SetText(points)
