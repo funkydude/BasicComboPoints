0d71a5e83b2f814a4ad8c1a3ecaed0cee1005966
funkydude <funkydude@users.noreply.github.com>
2008-06-20 13:28:50 +0000

    BasicComboPoints: cleanups, re-arranged config, added some localization

--- BasicComboPoints.lua
+++ BasicComboPoints.lua
@@ -2,7 +2,7 @@
 --      Are you local?      --
 ------------------------------
 
-local _, class = UnitClass("player")
+local class = select(2, UnitClass("player"))
 if class == "ROGUE" or class == "DRUID" then
 	class = nil
 else
@@ -10,7 +10,7 @@ else
 	return
 end
 
-local GetComboPoints = GetComboPoints
+local GetComboPoints = _G.GetComboPoints
 local text = nil
 local display = nil
 local font = nil
@@ -31,26 +31,12 @@ local defaults = {
 	}
 }
 
-local BasicComboPoints = LibStub("AceAddon-3.0"):NewAddon("BasicComboPoints", "AceEvent-3.0")
+local BasicComboPoints = LibStub("AceAddon-3.0"):NewAddon("BasicComboPoints")
 local media = LibStub("LibSharedMedia-3.0")
 
 ------------------------------
 --           Config         --
 ------------------------------
-
-local function setLock()
-	if not db.lock then
-		display:SetBackdropColor(1,1,1,0)
-		display:EnableMouse(false)
-		display:SetMovable(false)
-		db.lock = true
-	else
-		display:SetBackdropColor(1,1,1,1)
-		display:EnableMouse(true)
-		display:SetMovable(true)
-		db.lock = nil
-	end
-end
 
 local bcpOptions
 local function getOptions()
@@ -58,42 +44,189 @@ local function getOptions()
 		bcpOptions = {
 			type = "group",
 			name = "BasicComboPoints",
+			childGroups = "tab",
 			args = {
-				intro = {
-					type = "description",
-					name = "BasicComboPoints is a numerical display of your current combo points, with extras such as a font and color chooser.",
+				main = {
+					name = _G["MISCELLANEOUS"],
+					type = "group",
 					order = 1,
+					args = {
+						intro = {
+							type = "description",
+							name = "BasicComboPoints is a numerical display of your current combo points, with extras such as a font and color chooser.",
+							order = 1,
+						},
+						spacer = {
+							type = "header",
+							name = "",
+							order = 2,
+						},
+						lock = {
+							name = _G["LOCK"],
+							desc = "Lock the points frame in its current location.",
+							type = "toggle",
+							get = function() return db.lock end,
+							set = function(_, state) db.lock = state
+								if not state then
+									display:SetBackdropColor(1,1,1,1)
+									state = true
+								else
+									display:SetBackdropColor(1,1,1,0)
+									state = nil
+								end
+								display:EnableMouse(state)
+								display:SetMovable(state)
+							end,
+							order = 3,
+						},
+						font = {
+							desc = "Apply the font you wish to use for your Combo Points.",
+							type = "select", 
+							name = "Font",
+							values = media:List("font"),
+							get = function()
+								for k, v in pairs(media:List("font")) do
+									if db.font == v then
+										return k
+									end
+								end
+							end,
+							set = function(_, font)
+								db.font = media:List("font")[font]
+								BasicComboPoints:SetFont()
+							end,
+							order = 4,
+						},
+					},
 				},
-				spacer = {
-					type = "header",
-					name = "",
+				color = {
+					name = _G["COLOR"],
+					type = "group",
 					order = 2,
+					args = {
+						desc = {
+							type = "description",
+							name = "Apply the color you wish to use for your Combo Points.",
+							order = 1,
+						},
+						spacer = {
+							type = "header",
+							name = "",
+							order = 2,
+						},
+						colorone = {
+							desc = "Apply the color you wish to use for Combo Point 1.",
+							order = 3,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 1),
+							type = "color",
+							get = function() return db.colorone.r, db.colorone.g, db.colorone.b end,
+							set = function(_, r, g, b) db.colorone.r = r db.colorone.g = g db.colorone.b = b end,
+							width = "full",
+						},
+						colortwo = {
+							desc = "Apply the color you wish to use for Combo Point 2.",
+							order = 4,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 2),
+							type = "color",
+							get = function() return db.colortwo.r, db.colortwo.g, db.colortwo.b end,
+							set = function(_, r, g, b) db.colortwo.r = r db.colortwo.g = g db.colortwo.b = b end,
+							width = "full",
+						},
+						colorthree = {
+							desc = "Apply the color you wish to use for Combo Point 3.",
+							order = 5,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 3),
+							type = "color",
+							get = function() return db.colorthree.r, db.colorthree.g, db.colorthree.b end,
+							set = function(_, r, g, b) db.colorthree.r = r db.colorthree.g = g db.colorthree.b = b end,
+							width = "full",
+						},
+						colorfour = {
+							desc = "Apply the color you wish to use for Combo Point 4.",
+							order = 6,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 4),
+							type = "color",
+							get = function() return db.colorfour.r, db.colorfour.g, db.colorfour.b end,
+							set = function(_, r, g, b) db.colorfour.r = r db.colorfour.g = g db.colorfour.b = b end,
+							width = "full",
+						},
+						colorfive = {
+							desc = "Apply the color you wish to use for Combo Point 5.",
+							order = 7,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 5),
+							type = "color",
+							get = function() return db.colorfive.r, db.colorfive.g, db.colorfive.b end,
+							set = function(_, r, g, b) db.colorfive.r = r db.colorfive.g = g db.colorfive.b = b end,
+							width = "full",
+						},
+					},
 				},
-				lock = {
-					name = "Lock",
-					desc = "Lock the points frame in its current location.",
-					type = "toggle",
-					get = function() return db.lock end,
-					set = setLock,
+				size = {
+					name = _G["FONT_SIZE"],
+					type = "group",
 					order = 3,
-				},
-				font = {
-					desc = "Apply the font you wish to use for your Combo Points.",
-					type = "select", 
-					name = "Font",
-					values = media:List("font"),
-					get = function()
-						for k, v in pairs(media:List("font")) do
-							if db.font == v then
-								return k
-							end
-						end
-					end,
-					set = function(_, font)
-						db.font = media:List("font")[font]
-						BasicComboPoints:SetFont()
-					end,
-					order = 4,
+					args = {
+						desc = {
+							type = "description",
+							name = "Apply the size you wish to use for your Combo Points.",
+							order = 1,
+						},
+						spacer = {
+							type = "header",
+							name = "",
+							order = 2,
+						},
+						sizeone = {
+							desc = "Apply the size you wish to use for Combo Point 1.",
+							order = 3,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 1),
+							type = "range",
+							min = 1, max = 48, step = 1,
+							get = function() return db.size.one end,
+							set = function(_, v) db.size.one = v end,
+							width = "full",
+						},
+						sizetwo = {
+							desc = "Apply the size you wish to use for Combo Point 2.",
+							order = 4,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 2),
+							type = "range",
+							min = 1, max = 48, step = 1,
+							get = function() return db.size.two end,
+							set = function(_, v) db.size.two = v end,
+							width = "full",
+						},
+						sizethree = {
+							desc = "Apply the size you wish to use for Combo Point 3.",
+							order = 5,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 3),
+							type = "range",
+							min = 1, max = 48, step = 1,
+							get = function() return db.size.three end,
+							set = function(_, v) db.size.three = v end,
+							width = "full",
+						},
+						sizefour = {
+							desc = "Apply the size you wish to use for Combo Point 4.",
+							order = 6,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 4),
+							type = "range",
+							min = 1, max = 48, step = 1,
+							get = function() return db.size.four end,
+							set = function(_, v) db.size.four = v end,
+							width = "full",
+						},
+						sizefive = {
+							desc = "Apply the size you wish to use for Combo Point 5.",
+							order = 7,
+							name = format(_G["COMBAT_TEXT_COMBO_POINTS"], 5),
+							type = "range",
+							min = 1, max = 48, step = 1,
+							get = function() return db.size.five end,
+							set = function(_, v) db.size.five = v end,
+							width = "full",
+						},
+					},
 				},
 			},
 		}
@@ -101,184 +234,25 @@ local function getOptions()
 	return bcpOptions
 end
 
-local bcpOptionsColor
-local function getBcpColor()
-	if not bcpOptionsColor then
-		bcpOptionsColor = {
-			name = "Color",
-			type = "group",
-			args = {
-				desc = {
-					type = "description",
-					name = "Apply the color you wish to use for your Combo Points.",
-					order = 1,
-				},
-				spacer = {
-					type = "header",
-					name = "",
-					order = 2,
-				},
-				colorone = {
-					desc = "Apply the color you wish to use for Combo Point 1.",
-					order = 3,
-					name = "Combo Point 1",
-					type = "color",
-					get = function() return db.colorone.r, db.colorone.g, db.colorone.b end,
-					set = function(_, r, g, b) db.colorone.r = r db.colorone.g = g db.colorone.b = b end,
-					width = "full",
-				},
-				colortwo = {
-					desc = "Apply the color you wish to use for Combo Point 2.",
-					order = 4,
-					name = "Combo Point 2",
-					type = "color",
-					get = function() return db.colortwo.r, db.colortwo.g, db.colortwo.b end,
-					set = function(_, r, g, b) db.colortwo.r = r db.colortwo.g = g db.colortwo.b = b end,
-					width = "full",
-				},
-				colorthree = {
-					desc = "Apply the color you wish to use for Combo Point 3.",
-					order = 5,
-					name = "Combo Point 3",
-					type = "color",
-					get = function() return db.colorthree.r, db.colorthree.g, db.colorthree.b end,
-					set = function(_, r, g, b) db.colorthree.r = r db.colorthree.g = g db.colorthree.b = b end,
-					width = "full",
-				},
-				colorfour = {
-					desc = "Apply the color you wish to use for Combo Point 4.",
-					order = 6,
-					name = "Combo Point 4",
-					type = "color",
-					get = function() return db.colorfour.r, db.colorfour.g, db.colorfour.b end,
-					set = function(_, r, g, b) db.colorfour.r = r db.colorfour.g = g db.colorfour.b = b end,
-					width = "full",
-				},
-				colorfive = {
-					desc = "Apply the color you wish to use for Combo Point 5.",
-					order = 7,
-					name = "Combo Point 5",
-					type = "color",
-					get = function() return db.colorfive.r, db.colorfive.g, db.colorfive.b end,
-					set = function(_, r, g, b) db.colorfive.r = r db.colorfive.g = g db.colorfive.b = b end,
-					width = "full",
-				},
-			},
-		}
-	end
-	return bcpOptionsColor
-end
-
-local bcpOptionsSize
-local function getBcpSize()
-	if not bcpOptionsSize then
-		bcpOptionsSize = {
-			name = "Size",
-			type = "group",
-			args = {
-				desc = {
-					type = "description",
-					name = "Apply the size you wish to use for your Combo Points.",
-					order = 1,
-				},
-				spacer = {
-					type = "header",
-					name = "",
-					order = 2,
-				},
-				sizeone = {
-					desc = "Apply the size you wish to use for Combo Point 1.",
-					order = 3,
-					name = "Combo Point 1",
-					type = "range",
-					min = 1, max = 48, step = 1,
-					get = function() return db.size.one end,
-					set = function(_, v) db.size.one = v end,
-					width = "full",
-				},
-				sizetwo = {
-					desc = "Apply the size you wish to use for Combo Point 2.",
-					order = 4,
-					name = "Combo Point 2",
-					type = "range",
-					min = 1, max = 48, step = 1,
-					get = function() return db.size.two end,
-					set = function(_, v) db.size.two = v end,
-					width = "full",
-				},
-				sizethree = {
-					desc = "Apply the size you wish to use for Combo Point 3.",
-					order = 5,
-					name = "Combo Point 3",
-					type = "range",
-					min = 1, max = 48, step = 1,
-					get = function() return db.size.three end,
-					set = function(_, v) db.size.three = v end,
-					width = "full",
-				},
-				sizefour = {
-					desc = "Apply the size you wish to use for Combo Point 4.",
-					order = 6,
-					name = "Combo Point 4",
-					type = "range",
-					min = 1, max = 48, step = 1,
-					get = function() return db.size.four end,
-					set = function(_, v) db.size.four = v end,
-					width = "full",
-				},
-				sizefive = {
-					desc = "Apply the size you wish to use for Combo Point 5.",
-					order = 7,
-					name = "Combo Point 5",
-					type = "range",
-					min = 1, max = 48, step = 1,
-					get = function() return db.size.five end,
-					set = function(_, v) db.size.five = v end,
-					width = "full",
-				},
-			},
-		}
-	end
-	return bcpOptionsSize
-end
-
 ------------------------------
 --      Initialization      --
 ------------------------------
-
-local function openConfig()
-	InterfaceOptionsFrame_OpenToFrame("BasicComboPoints")
-end
 
 function BasicComboPoints:OnInitialize()
 	self.db = LibStub("AceDB-3.0"):New("BasicComboPointsDB", defaults)
 	db = self.db.profile
 
 	LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("BasicComboPoints", getOptions)
-	LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("BasicComboPointsColor", getBcpColor)
-	LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("BasicComboPointsSize", getBcpSize)
-
 	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BasicComboPoints", "BasicComboPoints")
-	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BasicComboPointsColor", "Color", "BasicComboPoints")
-	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BasicComboPointsSize", "Size", "BasicComboPoints")
 
-	_G["SlashCmdList"]["BASICCOMBOPOINTS_MAIN"] = openConfig
+	_G["SlashCmdList"]["BASICCOMBOPOINTS_MAIN"] = function() InterfaceOptionsFrame_OpenToFrame("BasicComboPoints") end
 	_G["SLASH_BASICCOMBOPOINTS_MAIN1"] = "/bcp"
-	_G["SlashCmdList"]["BASICCOMBOPOINTS_ALT"] = openConfig
-	_G["SLASH_BASICCOMBOPOINTS_ALT1"] = "/basiccombopoints"
+	_G["SLASH_BASICCOMBOPOINTS_MAIN2"] = "/basiccombopoints"
 end
 
 ------------------------------
 --       Frame Setup        --
 ------------------------------
-
-local function move() this:StartMoving() end
-local function stop()
-	this:StopMovingOrSizing()
-	local s = display:GetEffectiveScale()
-	db.x = display:GetLeft() * s
-	db.y = display:GetTop() * s
-end
 
 function BasicComboPoints:OnEnable()
 	display = CreateFrame("Frame", "BCPFrame", UIParent)
@@ -291,8 +265,13 @@ function BasicComboPoints:OnEnable()
 	display:SetHeight(50)
 	display:Show()
 	display:RegisterForDrag("LeftButton")
-	display:SetScript("OnDragStart", move)
-	display:SetScript("OnDragStop", stop)
+	display:SetScript("OnDragStart", function() this:StartMoving() end)
+	display:SetScript("OnDragStop", function() this:StopMovingOrSizing()
+		local s = display:GetEffectiveScale()
+		db.x = display:GetLeft() * s
+		db.y = display:GetTop() * s
+	end)
+	display:SetScript("OnEvent", function() BasicComboPoints:Update() end)
 	if not db.lock then
 		display:SetBackdropColor(1,1,1,1)
 		display:EnableMouse(true)
@@ -305,15 +284,15 @@ function BasicComboPoints:OnEnable()
 		display:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", db.x / s, db.y / s)
 	end
 
-	text = display:CreateFontString(nil,"OVERLAY")
+	text = display:CreateFontString("BCPText", "OVERLAY")
 	text:SetPoint("CENTER", display, "CENTER")
 	font = media:Fetch("font", db.font)
 	text:SetShadowColor(0, 0, 0, 1)
 	text:SetShadowOffset(1, -1)
 	text:SetFont(font, 15)
 
-	self:RegisterEvent("PLAYER_COMBO_POINTS", "Update")
-	self:RegisterEvent("PLAYER_TARGET_CHANGED", "Update")
+	display:RegisterEvent("PLAYER_COMBO_POINTS")
+	display:RegisterEvent("PLAYER_TARGET_CHANGED")
 
 	media.RegisterCallback(self, "LibSharedMedia_Registered", "SetFont")
 	media.RegisterCallback(self, "LibSharedMedia_SetGlobal", "SetFont")
