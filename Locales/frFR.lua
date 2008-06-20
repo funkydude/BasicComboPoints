208d477ab569cb451b3dfc07ac43abca6d72b2e2
tborlee <tborlee@users.noreply.github.com>
2008-06-20 16:25:23 +0000

    BasicComboPoints: Added french localization

---
+++ Locales/frFR.lua
@@ -0,0 +1,14 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("BasicComboPoints","frFR")
+
+L["desc"] = "BasicComboPoints est un affichage numérique de vos points de combo actuels, avec quelques extras comme la sélection de la police et de la couleur."
+
+L["Lock the points frame in its current location."] = "Verrouille le cadre des points à sa position actuelle."
+
+L["Font"] = "Police d'écriture"
+L["Apply the font you wish to use for your Combo Points."] = "Détermine la police d'écriture à utiliser pour vos points de combo."
+
+L["Apply the color you wish to use for your Combo Points."] = "Détermine la couleur à utiliser pour vos points de combo."
+
+L["Apply the %s you wish to use for Combo Point %d."] = "Détermine la %s à utiliser quand vous avez %d |4point:points; de combo."
+
+L["Apply the size you wish to use for your Combo Points."] = "Détermine la taille de police à utiliser pour vos points de combo."
